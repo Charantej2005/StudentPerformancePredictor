1645bf4c4aeb87a2ca2a4f330abec8d8acb2a5a6
Charantej2005 <charantejnm428@gmail.com>
2026-07-29 16:07:45 +0530

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,1 +1,20 @@
+import pandas as pd
 
+
+def load_student_data(file_path):
+  """load students datasets from student.csv"""
+  try:
+    data=pd.read_csv(file_path)
+    return data
+  except FileNotFindError:
+    print("File not found.Check the file path.")
+    return None
+    
+def display_data_info(data):
+  """display basic information form the student dataset"""
+  print("\n First 5 student records:")
+  print(data.head())
+  
+  data.info()
+  print("\n Stastistical summary")
+  print(data.describe())
