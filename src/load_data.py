01b1cf74e035d505a453ff9a0db1e0f819b4f8bb
Charantej2005 <charantejnm428@gmail.com>
2026-07-30 18:51:41 +0530

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 
-def load_student_data(file_path):
+def load_students_data(file_path):
   """load students datasets from student.csv"""
   try:
     data=pd.read_csv(file_path)
     return data
-  except FileNotFindError:
+  except FileNotFoundError:
     print("File not found.Check the file path.")
     return None
     
