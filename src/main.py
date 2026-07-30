e47c714b68cf2485226dd740e0277ab44a8f6428
Charantej2005 <charantejnm428@gmail.com>
2026-07-30 19:51:49 +0530

    Integrate data cleaning module with main program

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
 from load_data import load_students_data,display_data_info
+from clean_data import(remove_duplicates,check_missing_values,fill_missing_values,validate_marks)
 
 def main():
   file_path="data/raw/students.csv"
   students=load_students_data(file_path)
+  
   if students is not None:
     printf("loaded student dataset sucessfully.")
+    
+    """
+    from the line 15 to 18 ,code is written to  clean students data using the clean_data functions.
+    """
+    
+    students=remove_duplicates(students)
+    check_missing_values(students)
+    students=fill_missing_value(students)
+    students=validate_marks(students)
+    
     display_data_info(students)
+    
   else:
     print("Unable to load student dataset.")
 
