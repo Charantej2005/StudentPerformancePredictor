d3d30ed21cd6b64fb3605f63ac9c627a6ddfaeed
Charantej2005 <charantejnm428@gmail.com>
2026-07-30 18:07:19 +0530

    Connect main program with data loading module

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+from load_data import load_student_data,display_student_info
 
+def main():
+  file_path="data/raw/student.csv"
+  students=load_student_data(file_path)
+  if students is not None:
+    printf("loaded student dataset sucessfully.")
+    display_student_info(stuydents)
+  else:
+    print("Unable to load student dataset.")
+
+if __name__=="__main__":
+  main()
