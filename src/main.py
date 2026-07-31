1ff34c3e770724f19b0cbcbb68405fc74fe94401
Charantej2005 <charantejnm428@gmail.com>
2026-07-31 22:32:08 +0530

    Integrate analysis module with main program

--- src/main.py
+++ src/main.py
@@ -1,26 +1,44 @@
-from load_data import load_students_data,display_data_info
-from clean_data import(remove_duplicates,check_missing_values,fill_missing_values,validate_marks)
+from load_data import load_students_data, display_data_info
+from clean_data import (
+    remove_duplicates,
+    check_missing_values,
+    fill_missing_values,
+    validate_marks
+)
+from analysis import (
+    dataset_analysis,
+    numerical_summary,
+    missing_values,
+    unique_value
+)
+
 
 def main():
-  file_path="data/raw/students.csv"
-  students=load_students_data(file_path)
-  
-  if students is not None:
-    printf("loaded student dataset sucessfully.")
-    
-    """
-    from the line 15 to 18 ,code is written to  clean students data using the clean_data functions.
-    """
-    
-    students=remove_duplicates(students)
-    check_missing_values(students)
-    students=fill_missing_value(students)
-    students=validate_marks(students)
-    
-    display_data_info(students)
-    
-  else:
-    print("Unable to load student dataset.")
+    file_path = "data/raw/students.csv"
 
-if __name__=="__main__":
-  main()
+    students = load_students_data(file_path)
+
+    if students is not None:
+        print("Student dataset loaded successfully.")
+
+        # Clean the dataset
+        students = remove_duplicates(students)
+        check_missing_values(students)
+        students = fill_missing_values(students)
+        students = validate_marks(students)
+
+        # Analyze the dataset
+        dataset_analysis(students)
+        missing_values(students)
+        unique_value(students)
+        numerical_summary(students)
+
+        # Display dataset information
+        display_data_info(students)
+
+    else:
+        print("Unable to load student dataset.")
+
+
+if __name__ == "__main__":
+    main()
